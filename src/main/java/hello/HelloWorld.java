5684446cf3946076d30bad8b65b8eb240b123978
priyankmehta17 <priyankmehta17@gmail.com>
2018-07-28 17:18:06 +0530

    Testing Continuous Integration

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -6,6 +6,8 @@ public class HelloWorld {
     public static void main(String[] args) {
       LocalTime currentTime = new LocalTime();
 		  System.out.println("The current time is: " + currentTime);
+		  System.out.println("Hope you ll are having a nice time" );
+		  
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
